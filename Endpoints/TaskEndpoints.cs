eeae5ae27ad7200f7902958d064a56671eaf4463
Nathan Faria <nathanfaria607@gmail.com>
2023-06-02 15:25:32 -0300

    feat: add TaskEndpoints and add in the method MapTaskEndpoints the app.MapGet all tasks

---
+++ Endpoints/TaskEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using Dapper.Contrib.Extensions;
+using static ApiTask.Data.TaskContext;
+
+namespace ApiTask.Endpoints
+{
+    public static class TaskEndpoints
+    {
+        public static void MapTaskEndpoints(this WebApplication app)
+        {
+            app.MapGet("/", () => $"Welcome to the Task API {DateTime.Now}");
+
+            app.MapGet("/tasks", async (GetConnection connectionGetter) =>
+            {
+                using var con = await connectionGetter();
+                var tasks = con.GetAll<Task>().ToList();
+                if (tasks is null) return Results.NotFound();
+                return Results.Ok(tasks);
+            });
+        }
+    }
+}
