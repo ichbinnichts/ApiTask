1c9bdbec55a2eea72882ae0498b92615fc58d266
Nathan Faria <nathanfaria607@gmail.com>
2023-06-02 15:05:35 -0300

    feat: add ServiceCollectionsExtensions

---
+++ Data/TaskContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace ApiTask.Data
+{
+    public class TaskContext
+    {
+        public delegate Task<IDbConnection> GetConnection();
+    }
+}
---
+++ Extensions/ServiceCollectionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+using static ApiTask.Data.TaskContext;
+
+namespace ApiTask.Extensions
+{
+    public static class ServiceCollectionsExtensions
+    {
+        public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddScoped<GetConnection>(sp => async () =>
+            {
+                var connection = new NpgsqlConnection(connectionString);
+                await connection.OpenAsync();
+                return connection;
+            });
+            return builder;
+        }
+    }
+}
