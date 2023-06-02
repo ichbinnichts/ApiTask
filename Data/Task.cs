a7b83220f949a3f658b46b8f48ce9263c86a2196
Nathan Faria <nathanfaria607@gmail.com>
2023-06-02 14:37:44 -0300

    feat: add Task record

---
+++ Data/Task.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiTask.Data
+{
+    [Table("Task")]
+    public record Task(int Id, string Activity, string Status)
+    {
+    }
+}
